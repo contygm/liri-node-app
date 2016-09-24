7ccf955ae42747dae69ef7d0ae3c0e2a76421f55
Genevieve Conty <gmireillec@guest-2-00300.dhcp.unc.edu>
2016-09-24 11:23:24 -0400

    done

--- liri.js
+++ liri.js
@@ -10,22 +10,42 @@ var input = process.argv.slice(2);
 
 // get command
 
-switch (input[0]){
-	case "tweet":
-	case "my-tweets":
-		getTwitter(input[1]);
-		break;
-	
-	case "spotify":
-	case "spotify-this-song":
-		if (input[1]){
-			getSpotify(input[1]);
-		} else {
-			getSpotify("Ace of Base - The Sign");
-		}
-		break;
+function check() {
+		switch (input[0]){
+		case "tweet":
+		case "my-tweets":
+			getTwitter(input[1]);
+			break;
+		
+		case "spotify":
+		case "spotify-this-song":
+			if (input[1]){
+				getSpotify(input[1]);
+			} else {
+				getSpotify("Ace of Base - The Sign");
+			}
+			break;
+
+		case "movie":
+		case "movie-this":	
+			if (input[1]){
+				getMovie(input[1]);
+			} else {
+				getMovie("Mr. Nobody");
+			}
+			break;
+
+		case "do":
+		case "do-what-it-says":
+			doIt();
+			break;
+
+		default:
+	    console.log("Whatchu want holmes?");
+	}
 }
 
+check();
 
 // my tweets
 function getTwitter() {
@@ -43,7 +63,6 @@ function getTwitter() {
 
 	client.get('statuses/user_timeline', params, 	 
 		function(error, data, response){
-		    console.log('4');
 		    if(error) {
 		      throw error;
 		    }
@@ -55,9 +74,8 @@ function getTwitter() {
 		        data[i].created_at + "\n" + 
 		        "--------------\n";
 		      	console.log(myTweets);
-		      	// logData(myTweets);
-		    }
-		    
+		      	logIt(myTweets);
+		    }   
 	});
 }
 
@@ -65,7 +83,6 @@ function getTwitter() {
 // spotify this song
 
 function getSpotify(query){
-
 	spotify.search({ type: 'track', query: query}, function(error, data) {
 	    if (error) {
 	        throw error
@@ -77,29 +94,54 @@ function getSpotify(query){
 	      "Album: " + spotInfo.album.name + "\n" +
 	      "Preview Link: " +  spotInfo.preview_url + "\n";
 	    console.log(results);
-	    // logData(spotifyResults);
+	    logIt(results);
 	});
-
 }
 
 // movie-this
 
- //		* Title of the movie.
- //    * Year the movie came out.
- //    * IMDB Rating of the movie.
- //    * Country where the movie was produced.
- //    * Language of the movie.
- //    * Plot of the movie.
- //    * Actors in the movie.
- //    * Rotten Tomatoes Rating.
- //    * Rotten Tomatoes URL.
+ function getMovie(query){
+
+	var url = 'http://www.omdbapi.com/?t=' + query + '&y=&plot=short&r=json&tomatoes=true';
+	request(url, function (error, response, body) {
+
+		if (!error && response.statusCode == 200) {
+    		var results = 
+			    "Title: " + JSON.parse(body)["Title"] + "\n" +
+			    "Year Released: " + JSON.parse(body)["Year"] + "\n" +
+			    "IMDB Rating: " +  JSON.parse(body)["imdbRating"] + "\n" +
+			    "Country of Production: " + JSON.parse(body)["Country"] + "\n" +
+			   	"Language: " + JSON.parse(body)["Language"] + "\n" +
+			    "Plot: " + JSON.parse(body)["Plot"] + "\n" +
+			    "Actors: " + JSON.parse(body)["Actors"] + "\n" +
+			    "Rotten Tomatoes Rating: " + JSON.parse(body)["tomatoRating"] + "\n" +
+			    "Rotten Tomatoes URL: " + JSON.parse(body)["tomatoURL"] + "\n";
+
+			    console.log(results);
+			    logIt(results);
+		}
+	});
+}
 
 // do what it says
 
+function doIt(){
+	fs.readFile("./random.txt", "utf8", function(error, data){
+		if (error) {
+	        throw error;
+	    }
+	    data = data.split(',');
+	    input[0] = data[0];
+	    input[1] = data[1];
+	    check();
+	});
+}
 
-
-
-
-
-
+function logIt(results) {
+  fs.appendFile("./log.txt", results, (error) => {
+    if(error) {
+      throw error;
+    }
+  });
+}
 
