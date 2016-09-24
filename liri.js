d6ffe86cd5c9c0cfd534c139d18f8185939d06bb
Genevieve Conty <gmireillec@Genevieves-MacBook-Pro.local>
2016-09-24 06:53:28 -0400

    connected key files

---
+++ liri.js
@@ -0,0 +1,15 @@
+
+// get keys
+
+// var fs = require('fs');
+var keys = require("./keys.js");
+
+console.log(keys.twitterKeys.consumer_key);
+
+// my tweets
+
+// spotify this song
+
+// movie-this
+
+// do what it says
